f680817ad231b40e05031f4c827eff13e7b4774d
alex <alexander.bollbach@gmail.com>
2017-03-05 11:42:18 -0500

    initial commit

---
+++ Example/Pods/Target Support Files/ABSegmentControl/ABSegmentControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ABSegmentControl : NSObject
+@end
+@implementation PodsDummy_ABSegmentControl
+@end
---
+++ Example/Pods/Target Support Files/Pods-ABSegmentControl_Example/Pods-ABSegmentControl_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ABSegmentControl_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ABSegmentControl_Example
+@end
